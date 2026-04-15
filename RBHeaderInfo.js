dc4a0ab724fb89eaceb7e1a35bccd6c86aa1f02e
Patrick Reinhart <60241477+bartimaeusrex@users.noreply.github.com>
2026-04-15 10:00:32 -0600

    Trying to get the favicon to show
    fleuron.ico hasn't been showing up locally or online, when I have it in the images folder. Maybe in the main folder will do the trick.

--- RBHeaderInfo.js
+++ RBHeaderInfo.js
@@ -13,7 +13,7 @@ class RBHeaderInfo extends HTMLElement {
 
   <link href="styles.css" rel="stylesheet" />
   <link href="basics.css" rel="stylesheet" />
-  <link rel="shortcut icon" type="image/x-icon" href="images/fleuron.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="fleuron.ico">
 
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Press+Start+2P&family=Courier+Prime:ital@0;1&family=Cutive+Mono&family=Hammersmith+One&family=Lexend+Tera&family=EB+Garamond:ital,wght@0,400;0,600;0,800;1,400;1,800&family=Montserrat:ital,wght@0,200;0,700;0,900;1,200;1,700&family=Sansita&family=VT323&family=Grenze+Gotisch:wght@100..900&display=swap" rel="stylesheet">
